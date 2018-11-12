ef5e246c2e8fef51a5a5506d32f203b942a814cd
brieuc l'hélias <brieuc.lhelias@gmail.com>
2018-11-12 08:42:45 +0100

    remove files

--- Regression/__init__.py
+++
@@ -1,9 +0,0 @@
-from .linear_regression import LinReg
-from .logistic_regression import LogReg
-from .multivariate_regression import MultiReg
-
-__all__ = [
-    'LinReg',
-    'LogReg',
-    'MultiReg'
-]
